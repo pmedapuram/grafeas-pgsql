d57e8bf8dfde7c91d29e86268858d17d53c3f6f6
Pavan Tej Medapuram <pmedapuram@salesforce.com>
2020-03-09 21:53:28 -0700

    add swagger and fix postgres password

--- postgres/init.sh
+++ postgres/init.sh
@@ -5,8 +5,8 @@ echo 'Setting up PostgreSQL database'
 
 # Replace with extracting username and password from config
 # Set password and utilize username from extracted var
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE USER grafeas WITH PASSWORD 'changeme';
+psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --password "$POSTGRES_PASSWORD"<<-EOSQL
+    CREATE USER grafeas;
     ALTER ROLE grafeas WITH CREATEDB;
     CREATE DATABASE grafeas_db;
     GRANT ALL PRIVILEGES ON DATABASE grafeas_db TO grafeas;
